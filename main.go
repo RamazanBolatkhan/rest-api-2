4ada5a70fd55fc1e9f727acfdf8c4d123f707f30
RamazanB <ramazanbolathan04@gmail.com>
2022-08-08 17:18:17 +0600

    Patch toggle added

--- main.go
+++ main.go
@@ -44,6 +44,19 @@ func getTodoById(id string) (*todo, error) {
 	return nil, errors.New("todo not found")
 }
 
+func toggleTodoStatus(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+		return
+	}
+
+	todo.Completed = !todo.Completed
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
 func getTodo(context *gin.Context) {
 	id := context.Param("id")
 	todo, err := getTodoById(id)
@@ -55,10 +68,12 @@ func getTodo(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todo)
 
 }
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleTodoStatus)
 	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
